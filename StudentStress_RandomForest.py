4e179aad9851493686bb39c0b5cf85e1e43afeec
ajay-bhagava <ajay.bhagava@gmail.com>
2025-10-08 16:17:38 -0400

    added precision and recall calculations, use weighted average

--- StudentStress_RandomForest.py
+++ StudentStress_RandomForest.py
@@ -2,29 +2,38 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score
 from lightgbm import LGBMClassifier
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-def evaluate(y_test, y_pred):
+sns.set(font_scale=1.25)
+
+def evaluate(y_test, y_pred, model_name):
     # Get class labels for confusion matrix
     class_labels = sorted(y_test.unique().tolist())
     # Model Evaluation with Confusion Matrix
+    # Get metrics 
     accuracy = accuracy_score(y_test, y_pred)
     print(f'Accuracy: {accuracy * 100:.2f}%')
 
+    precision = precision_score(y_test, y_pred, average='weighted')
+    print(f'Precision: {precision * 100:.2f}%')
+
+    recall = recall_score(y_test, y_pred, average='weighted')
+    print(f'Recall: {recall * 100:.2f}%')
+
+    # Get confusion matrix and plot
     conf_matrix = confusion_matrix(y_test, y_pred, labels=class_labels)
 
-    plt.figure(figsize=(8, 6))
-    sns.heatmap(conf_matrix, annot=True, fmt='g', cmap='Blues', cbar=False, xticklabels=class_labels, yticklabels=class_labels)
+    plt.figure(model_name, figsize=(8, 6))
+    sns.heatmap(conf_matrix, annot=True, fmt='g', cmap='Blues', cbar=False, xticklabels=class_labels, yticklabels=class_labels, annot_kws={"size": 20})
 
-    plt.title('Confusion Matrix Heatmap')
+    plt.title(f'Confusion Matrix for {model_name}')
     plt.xlabel('Predicted Labels')
     plt.ylabel('True Labels')
     plt.tight_layout()
-    plt.show()
 
 
 # df = pd.read_csv(r'Stress_Dataset.csv')
@@ -42,7 +51,7 @@ class_labels = sorted(y.unique().tolist())
 # Partitioning Dataset into Test and Validation Sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=72)
 
-# Feature Scaling
+# Feature Scaling, scale to normal distribution mean=0, std.dev=1
 scaler = StandardScaler().set_output(transform="pandas")
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
@@ -52,7 +61,7 @@ classifier = RandomForestClassifier(n_estimators=100, random_state=42)
 classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
 print("Random Forest Classifier")
-evaluate(y_test, y_pred)
+evaluate(y_test, y_pred, "Random Forest")
 print("---------")
 
 # LGBM Classsifier
@@ -60,7 +69,7 @@ lgbm = LGBMClassifier(nestimators=100, random_state=42, verbosity=-1)
 lgbm.fit(X_train, y_train)
 y_pred = lgbm.predict(X_test)
 print("LGBM Classifier")
-evaluate(y_test, y_pred)
+evaluate(y_test, y_pred, "LightGBM")
 print("---------")
 
 # Feature Importance
@@ -76,7 +85,7 @@ for idx in indices:
 top_n = 8
 top_indices = indices[:top_n]
 
-plt.figure(figsize=(8,6))
+plt.figure("FeatureImportance", figsize=(8,6))
 plt.title(f'Top {top_n} Feature Importances')
 plt.bar(range(top_n), importances[top_indices], align='center')
 plt.xticks(range(top_n), feature_names[top_indices], rotation=45)
